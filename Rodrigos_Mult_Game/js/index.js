83c9b534aec629e924b5660662ecd5117192944b
Rauld1 <rdmusic@yahoo.com>
2014-11-30 12:25:32 -0800

    intial commit

---
+++ Rodrigos_Mult_Game/js/index.js
@@ -0,0 +1,6 @@
+// numbers across the top and left side
+// well which shows the problem and prompt for answer
+// when answer is correct do a good job! purring cat
+// if he gets it wrong it says: Oh no try again crying cat..
+//
+
